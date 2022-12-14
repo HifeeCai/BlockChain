6af424a5e6a65c52464eeba0334bd8ab5818daf0
HifeeCai <hifee.cai@mywaytex.com>
2022-12-14 10:42:13 +0800

    v1

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Printf("Welcome to BlockChain!\n")
+
+	//block := NewBlock(genesisInfo, []byte{0x0000000000000001})
+	//
+	//fmt.Printf("PrevBlockHash : %x\n", block.PrevBlockHash)
+	//fmt.Printf("Hash : %x\n", block.Hash)
+	//fmt.Printf("Data : %s\n", block.Data)
+
+}
